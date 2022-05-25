11046ec7073085e7edf51cc2f86bb35d73e1df3d
Afix42 <101334091+Afix42@users.noreply.github.com>
2022-05-24 20:08:56 -0400

    se dejo lista la base de datos, solo tiene berlin

--- Panaderia/core/admin.py
+++ Panaderia/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Rol,Usuario,Comuna,Direccion,Venta,Producto,Comentario,Detalle
 
 # Register your models here.
+admin.site.register(Rol)
+admin.site.register(Usuario)
+admin.site.register(Comuna)
+admin.site.register(Direccion)
+admin.site.register(Venta)
+admin.site.register(Producto)
+admin.site.register(Comentario)
+admin.site.register(Detalle)
--- Panaderia/core/models.py
+++ Panaderia/core/models.py
@@ -22,14 +22,14 @@ class Usuario(models.Model):
 
 class Comuna(models.Model):
     idComuna = models.IntegerField(primary_key=True, verbose_name='id de la comuna')
-    descripcionComuna = models.CharField(max_length=25, verbose_name='Descripcion de la comuna', blank=False, null=False)
+    descripcionComuna = models.CharField(max_length=100, verbose_name='Descripcion de la comuna', blank=False, null=False)
 
     def __str__(self):
         return self.descripcionComuna
 
 class Direccion(models.Model):
     idDireccion = models.IntegerField(primary_key=True, verbose_name='id de la direccion')
-    descripcionDireccion = models.CharField(max_length=25, verbose_name='Descripcion de la direccion', blank=False, null=False)
+    descripcionDireccion = models.CharField(max_length=100, verbose_name='Descripcion de la direccion', blank=False, null=False)
     usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)
     comuna = models.ForeignKey(Comuna,on_delete=models.CASCADE)
 
@@ -49,7 +49,7 @@ class Venta(models.Model):
 class Producto(models.Model):
     idProducto = models.IntegerField(primary_key=True, verbose_name='id del producto')
     nombreProducto = models.CharField(max_length=25, verbose_name='Nombre del producto', blank=False, null=False)
-    descripcionProducto = models.CharField(max_length=25, verbose_name='Descripcion del producto', blank=False, null=False)
+    descripcionProducto = models.CharField(max_length=100, verbose_name='Descripcion del producto', blank=False, null=False)
     foto = models.ImageField(upload_to="producto", null= True)
     total = models.IntegerField(verbose_name='Precio del producto')
     stock = models.IntegerField(verbose_name='Stock del producto')
@@ -59,7 +59,7 @@ class Producto(models.Model):
 
 class Comentario(models.Model):
     idComentario = models.IntegerField(primary_key=True, verbose_name='id del comentario')
-    descripcionComentario = models.CharField(max_length=25, verbose_name='Descripcion del comentario', blank=False, null=False)
+    descripcionComentario = models.CharField(max_length=100, verbose_name='Descripcion del comentario', blank=False, null=False)
     baneo = models.IntegerField(verbose_name='Estado del baneo')
     usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto,on_delete=models.CASCADE)
