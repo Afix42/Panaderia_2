f5736df1d348394ac46bb956bcc75b941cf039b3
fel-canto <fel.canto@duocuc.cl>
2022-06-17 22:05:47 -0400

    Api CREADA
    Serializadores: Producto, Usuario y Rol.

---
+++ Panaderia/api_rest/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from api_rest.views import agregarProducto, EliminarModificarProducto, listarProductos, listarUsuarios, listarRol
+
+
+urlpatterns = [
+    path('agregarProducto', agregarProducto, name='agregarProducto'),
+    path('EliminarModificarProducto/<id>',EliminarModificarProducto,name='EliminarModificarProducto'),
+    path('listarProductos', listarProductos, name='listarProductos'),
+    path('listarUsuarios', listarUsuarios, name='listarUsuarios'),
+    path('listarRol', listarRol, name='listarRol')
+]
